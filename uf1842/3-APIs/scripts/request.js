f5459e4b22612cc2c71a9921a3656cb5e20dbcd7
MariaChmielinska <maria.anna.chmielinska@gmail.com>
2024-06-08 13:49:07 +0200

    intento de API

--- uf1842/3-APIs/scripts/request.js
+++ uf1842/3-APIs/scripts/request.js
@@ -1,5 +1,22 @@
 async function getPuzzle(wordCount) {
     let hiddenSentence = "Buena Suerte";
 
+    let url = "https://puzzle.mead.io/puzzle";
+     if(wordCount.value){
+        url += "?wordCount=" + wordCount.value;  
+     }
+
+    let response = await fetch(url, {
+        method: 'GET',
+        headers:{
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        
+    }
+        );
+     console.log("response works", response);
+    let data = await response.json();
+     console.log("data shows",data);
+
     return hiddenSentence;
 }
