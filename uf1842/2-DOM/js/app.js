5f7c2cb72b52213ed6e48f1d4d6f2522b4e06e4d
MariaChmielinska <maria.anna.chmielinska@gmail.com>
2024-06-08 12:52:25 +0200

    done

--- uf1842/2-DOM/js/app.js
+++ uf1842/2-DOM/js/app.js
@@ -1,3 +1,24 @@
 /**
  * ¡Buena suerte! ¡Pasa el mensaje!
  */
+
+
+button = document.querySelector("#btn-send");
+button.addEventListener("click",send);
+
+    function send(){
+        console.log("button Enviar works");
+
+        let message = document.querySelector("#message").value;
+        console.log("input message shows", message);
+
+        document.querySelector("#last-message").textContent = message;
+        console.log("last message shows", lastMessage);
+
+        message = "";
+
+
+    }
+
+
+
